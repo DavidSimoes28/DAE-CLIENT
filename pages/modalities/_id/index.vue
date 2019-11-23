9b89fb18d7a1ed773f8107372f94b6194f76a574
David Simões <2170978@my.ipleiria.pt>
2019-11-23 12:13:51 +0000

    asdasd

--- pages/modalities/_id/index.vue
+++ pages/modalities/_id/index.vue
@@ -3,6 +3,9 @@
     <h4>Modality Details:</h4>
     <p>Id: {{ modality.id }}</p>
     <p>Name: {{ modality.name }}</p>
+    <h4>Coaches enrolled:</h4>
+    <b-table v-if="coaches.length" striped over :items="coaches" :fields="fields" />
+    <p v-else>No coaches enrolled.</p>
     <nuxt-link to="/modalities">Back</nuxt-link>
   </b-container>
 </template>
@@ -11,11 +14,16 @@
         data() {
             return {
                 modality: {},
+                fields: ['username', 'name', 'email'],
+                coaches: []
             }
         },
         computed: {
             id() {
                 return this.$route.params.id
+            },
+            coaches() {
+                return this.modality.coaches || []
             }
         },
         created() {
